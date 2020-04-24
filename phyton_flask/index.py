fda520dc2ec6745944336e74d863dbc50aa217e9
pablo <pablotp9105@outlook.com>
2020-04-24 12:21:09 -0500

    crud

--- phyton_flask/index.py
+++ phyton_flask/index.py
@@ -1,21 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="taskapp"
+)
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def start():
-    return render_template('home.html')
+    mycursor = mydb.cursor()
+    sql = "SELECT * FROM tablatask"
+    mycursor.execute(sql)
+    result = mycursor.fetchall()
+    return render_template('home.html', tasks = result)
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/createtask')
+def createtask():
+    return render_template('create_task.html')
 
-@app.route('/registro')
-def registro():
-    return render_template('registro.html')
+@app.route('/addtask', methods=['POST'])
+def addtask():
+    if request.method =='POST':
+        taskname = request.form['taskname']
+        taskdate = request.form['taskdate']
+        print("taskname", taskname, "taskdate", taskdate)
+
+        mycursor = mydb.cursor()
+        sql = f"INSERT INTO tablatask (Task,Date) VALUES ('{taskname}', '{taskdate}')"
+        mycursor.execute(sql)
+        mydb.commit()
+        return redirect(url_for('start'))
+
+
+  
+    
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
